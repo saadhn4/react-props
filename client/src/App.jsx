82b75d00e56809b3aae04075608c15cd5691af9f
saadhn4 <saadcfi4@gmail.com>
2025-02-19 12:24:24 +0530

    done

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Card from "./components/Card";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello!</h1>
+      <Card name="Saad" />
+      <Card name="Rahman" />
+    </>
+  );
+};
+
+export default App;
---
+++ client/src/components/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Card = (props) => {
+  return (
+    <div>
+      <div>Hi My name is {props.name} </div>
+    </div>
+  );
+};
+
+export default Card;
